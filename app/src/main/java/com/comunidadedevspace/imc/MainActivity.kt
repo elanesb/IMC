81f1dbdd589dd5856c3afe51bb60f8116c39dc3c
Elane Barbosa <elane.biomed@gmail.com>
2025-02-21 14:57:59 -0300

    Mostrando resultado na proxima tela

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -36,6 +37,13 @@ class MainActivity : AppCompatActivity() {
 
                 val alturaQ2 = altura * altura
                 val resultado = peso / alturaQ2
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+
+                startActivity(intent)
+
+
                 println("Roque acao do botao" + resultado)
             }
         }
